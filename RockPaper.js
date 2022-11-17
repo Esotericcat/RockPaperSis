2132ba9750fc71a84e3ae1eede81d1a5dc133acc
Esotericcat <117365866+Esotericcat@users.noreply.github.com>
2022-11-17 16:16:20 +0100

    First step

--- RockPaper.js
+++ RockPaper.js
@@ -1,1 +1,8 @@
-swqdswadad
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+  
