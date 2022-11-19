6aa44a8a1174739f105cfc10569cfe088645a6d7
Esotericcat <117365866+Esotericcat@users.noreply.github.com>
2022-11-19 15:15:01 +0100

    Phase1 vs 1

--- RockPaper.js
+++ RockPaper.js
@@ -1,11 +1,14 @@
 function getComputerChoice() {
-    let num1 = Math.random() 
-     if (num1 > 0.33 && num1 <= 0.66 ) {
-        console.log('Rock') 
-     }else if (num1 <= 0.33) {
-        console.log('Paper')
-     }else  if (num1 > 0.66) {
-      console.log('Scissors') }
-      console.log(num1)
-        }
-    getComputerChoice()     
+   let num1 = Math.random() 
+    if (num1 > 0.33 && num1 <= 0.66 ) {
+      return 'Rock'
+    }else if (num1 <= 0.33) {
+       return 'Paper'
+    }else  if (num1 > 0.66) {
+     return 'Scissors' }
+     
+     
+     
+       }
+    getComputerChoice() 
+    console.log(getComputerChoice())   
